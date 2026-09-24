564c16bf7d2dac719369cfcbaa66366fdb9b96db
ZehraDuymaz <duymazzehra@gmail.com>
2026-09-24 14:49:37 +0300

    Laptop'taki küçük frontend'teki değişiklikler

--- src/Evenex.Application/obj/Debug/net10.0/Evenex.Application.AssemblyInfo.cs
+++ src/Evenex.Application/obj/Debug/net10.0/Evenex.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Evenex.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de3ac98bbac43e8e3f53afe4622535226ee84b58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48257c853767af8c49bbe058e33424c2ceca316a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Evenex.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Evenex.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
